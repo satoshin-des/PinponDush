e6584544f348f2386af5f4daa2f3e0d2e3bda097
Nagato <transcendentalliouville@gmail.com>
2025-02-25 00:10:57 +0900

    不要なファイルを削除

--- Main.cpp
+++ Main.cpp
@@ -13,9 +13,6 @@ void Main()
     int game_state = 0;                         // 今のゲームの状態
     int num_dushed_houses = 0;                  // ピンポンダッシュに成功した家の数
 
-    // Create a texture from an image file
-    // const Texture texture{ U"example/windmill.png" };
-
     game.setNumHouse(2);
 
     while (System::Update())
@@ -30,24 +27,5 @@ void Main()
             game_state = game.mainScreen();
             break;
         }
-
-        /*
-        // Draw a red transparent circle that follows the mouse cursor
-        Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
-
-        // When [A] key is down
-        if (KeyA.down())
-        {
-            // Print a randomly selected text
-            Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
-        }
-
-        // When [Button] is pushed
-        if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
-        {
-            // Move the coordinates to a random position in the screen
-            emojiPos = RandomVec2(Scene::Rect());
-        }
-        */
     }
 }
