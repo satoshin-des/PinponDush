c58e98ecdeebe614cee447243666875adb492eab
Nagato <transcendentalliouville@gmail.com>
2025-02-27 15:41:13 +0900

    ゲームオーバーBGMを追加

--- core.h
+++ core.h
@@ -7,7 +7,7 @@
 #define WIDTH_Y_HALF 300
 #define HOUSE_SIZE 150
 
-#define GAME_OVER_BGM U"resources/engine/03.wav"
+#define GAME_OVER_BGM U"bgm/03.wav"
 
 #define TITLE_SCREEN 0
 #define GAME_SCREEN 1
